de4eacdea9bc7eb4a9bf71c72503146109146383
Adi-hub-03 <aditya03walia@gmail.com>
2024-08-16 00:03:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let x=document.getElementById("Personal");
+x.addEventListener("mouseenter",function(){
+    x.style.fontSize="larger";
+    x.style.color="Black"
+});
+x.addEventListener("mouseleave",function(){
+    x.style.fontSize="large";
+});
+
+let y=document.getElementById("Ed");
+y.addEventListener("mouseenter",function(){
+    y.style.fontSize="larger";
+    y.style.color="Black"
+});
+y.addEventListener("mouseleave",function(){
+    y.style.fontSize="large";
+});
+
+let z=document.getElementById("Skill");
+z.addEventListener("mouseenter",function(){
+    z.style.fontSize="larger";
+    z.style.color="Black"
+});
+z.addEventListener("mouseleave",function(){
+    z.style.fontSize="large";
+});
+
+let a=document.getElementById("Exp");
+a.addEventListener("mouseenter",function(){
+    a.style.fontSize="larger";
+    a.style.color="Black"
+});
+a.addEventListener("mouseleave",function(){
+    a.style.fontSize="large";
+});
+
+let b=document.getElementById("Cert");
+b.addEventListener("mouseenter",function(){
+    b.style.fontSize="larger";
+    b.style.color="Black"
+});
+b.addEventListener("mouseleave",function(){
+    b.style.fontSize="large";
+});
+
+let c=document.getElementById("Learn");
+c.addEventListener("mouseenter",function(){
+    c.style.fontSize="larger";
+    c.style.color="Black"
+});
+c.addEventListener("mouseleave",function(){
+    c.style.fontSize="large";
+});
